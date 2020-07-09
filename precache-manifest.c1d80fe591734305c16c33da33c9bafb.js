36a7dd1f7e0a6673f9aac20d3280b358256aa7a3
Yash2906 <panchalyash.2906@gmail.com>
2020-07-09 12:45:49 +0530

    Updates

---
+++ precache-manifest.c1d80fe591734305c16c33da33c9bafb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c99fa1d992a1462dcab986a60838cfc",
+    "url": "/stripereact/index.html"
+  },
+  {
+    "revision": "60406b6c9ee9c26c7a2c",
+    "url": "/stripereact/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "16ea18788bdb3cc9b516",
+    "url": "/stripereact/static/js/2.8c966022.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/stripereact/static/js/2.8c966022.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60406b6c9ee9c26c7a2c",
+    "url": "/stripereact/static/js/main.ab2dcb0a.chunk.js"
+  },
+  {
+    "revision": "79956df9c0a3012f69e8",
+    "url": "/stripereact/static/js/runtime-main.39f73439.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/stripereact/static/media/logo.5d5d9eef.svg"
+  }
+]);
